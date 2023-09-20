47127028336f889bb3d1622615fd246a0f702553
Daniel-Soares-Franco <danielsfranco346@gmail.com>
2023-09-19 22:46:01 -0300

    modified:   Back-end/build/routes/Vaga.Routes.js 	modified:   Back-end/src Empcd 19-09/routes/Vaga.Routes.ts

--- Back-end/build/routes/Vaga.Routes.js
+++ Back-end/build/routes/Vaga.Routes.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const Vaga_controllers_1 = __importDefault(require("../controllers/Vaga.controllers"));
 const Vagarouter = (0, express_1.Router)();
-Vagarouter.post("/", new Vaga_controllers_1.default().createVaga);
-Vagarouter.get("/", new Vaga_controllers_1.default().getVagas);
-Vagarouter.get("/:idVaga", new Vaga_controllers_1.default().getVagaById);
+Vagarouter.post("/", new Vaga_controllers_1.default().createVaga); //ok
+Vagarouter.get("/", new Vaga_controllers_1.default().getVagas); //ok
+Vagarouter.get("/:idVaga", new Vaga_controllers_1.default().getVagaById); //ok
 exports.default = Vagarouter;
--- Back-end/src Empcd 19-09/routes/Vaga.Routes.ts
+++ Back-end/src Empcd 19-09/routes/Vaga.Routes.ts
@@ -3,8 +3,8 @@ import VagaController from "../controllers/Vaga.controllers";
 
 const Vagarouter = Router();
 
-Vagarouter.post("/", new VagaController().createVaga);
-Vagarouter.get("/", new VagaController().getVagas);
-Vagarouter.get("/:idVaga", new VagaController().getVagaById);
+Vagarouter.post("/", new VagaController().createVaga); //ok
+Vagarouter.get("/", new VagaController().getVagas); //ok
+Vagarouter.get("/:idVaga", new VagaController().getVagaById); //ok
 
 export default Vagarouter;
